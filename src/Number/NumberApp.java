5fb66d608302653badd723b6ee0c49ea3acbd95d
Erli <erli.elot@gmail.com>
2022-09-14 08:56:55 +0700

    up

--- src/Number/NumberApp.java
+++ src/Number/NumberApp.java
@@ -11,7 +11,32 @@ public class NumberApp {
         System.out.println(iniByte);
         System.out.println(iniShort);
         System.out.println(iniInteger);
-        System.out.println(iniLong);
+        System.out.println(iniLong+"\n");
+
+        // Floating Point
+
+        System.out.println("menggukan Floating poin");
+        float iniFloat =  1.0F; // gunkan huruf F dibelakang bila ingin menggukan titik
+        double iniDouble = 2D;
+        System.out.println(iniDouble);
+        System.out.println(iniFloat+ "\n");
+
+        System.out.println("Menggukan  Literals");
+        int decimal = 25;
+        int hexdecimal = 0X1F;
+        int binary = 0B0101010;
+
+        System.out.println(decimal);
+        System.out.println(hexdecimal);
+        System.out.println(binary+"\n");
+
+        // underscor
+        System.out.println("Menggnukan underScor");
+        long RP = 60_000_000;
+        int sum = 1_000;
+
+        System.out.println(RP);
+        System.out.println(sum);
 
     }
 }
