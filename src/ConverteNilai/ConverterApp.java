c4babbec9c74b00f67fa56b45748eaa814a32566
Erli <erli.elot@gmail.com>
2022-09-14 09:09:27 +0700

    up

---
+++ src/ConverteNilai/ConverterApp.java
@@ -0,0 +1,31 @@
+package ConverteNilai;
+
+public class ConverterApp {
+    public static void main(String[] args) {
+
+
+        // converter Otomatis
+        // converter nilai otomatis dari kecil ke besar
+
+        System.out.println("Conveter nilai Otomatis");
+        byte iniByte = 10;
+        short iniShort = iniByte;
+
+        int iniInteger = 20;
+        long iniLong = iniInteger;
+
+        System.out.println("Conveter nilai byte ke Short : " + iniShort);
+        System.out.println("Conveter nilai Int ke Long : " + iniLong + "\n");
+
+        System.out.println("Converter Nilai Manual");
+
+        double iniDouble = 10D;
+        int iniInteger_Satu = (int) iniDouble;
+
+        float iniFloate = 10f;
+        short iniShort_Satu = (short) iniFloate;
+
+        System.out.println("Converter manual Dari Double ke Integer : " + iniInteger_Satu);
+        System.out.println("Converter manual Dari Short ke Float : " + iniShort_Satu );
+    }
+}
