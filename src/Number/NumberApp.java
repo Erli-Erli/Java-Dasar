734135f46cb6f949084b8e543b399b0f4c0cc300
Erli <erli.elot@gmail.com>
2022-09-14 08:31:32 +0700

    up

---
+++ src/Number/NumberApp.java
@@ -0,0 +1,17 @@
+package Number;
+
+public class NumberApp {
+    public static void main(String[] args) {
+
+        byte iniByte = 10;
+        short iniShort = 1000;
+        int iniInteger = 100000;
+        long iniLong = 1000000000;
+
+        System.out.println(iniByte);
+        System.out.println(iniShort);
+        System.out.println(iniInteger);
+        System.out.println(iniLong);
+
+    }
+}
